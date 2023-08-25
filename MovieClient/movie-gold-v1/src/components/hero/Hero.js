2291081b0676741bf1e5316d8a66c629f3889b93
diogoraposo13 <diogo.raposo@live.com.pt>
2023-08-25 14:52:03 +0100

    Create Home and Hero components

---
+++ MovieClient/movie-gold-v1/src/components/hero/Hero.js
@@ -0,0 +1,36 @@
+import './Hero.css'
+import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import {Paper} from '@mui/material'
+
+const Hero = ({movies}) => {
+  return (
+    <div className='movie-carousel-container'>
+        <Carousel>
+            {
+                movies.map((movie) => {
+                    return(
+                        <Paper>
+                            <div className='movi-card-container'>
+                                <div className='movie-card'>
+                                    <div className='movie-detail'>
+                                        <div className='movie-poster'>
+                                            <img src={movie.poster} alt=""/>
+                                        </div>
+                                        <div className='movie-title'>
+                                            <h4>{movie.title}</h4>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </Paper>
+                    )
+                })
+            }
+        </Carousel>
+    </div>
+  )
+}
+
+export default Hero
